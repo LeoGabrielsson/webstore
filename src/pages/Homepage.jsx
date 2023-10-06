157e25acb8fe2d6c1c9a4fd08c13564e0f7abfad
= <=>
2023-10-06 16:06:59 +0200

    Grejs

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -12,7 +12,7 @@ function Homepage() {
 
                 <div className="bestProducts">
                     <section className="pCard">
-                        <img className="pImage" src="imgs/shirt.jpg" alt="Product Name" />
+                        <img className="pImage" src="./src/imgs/hero.jpg" alt="Product Name" />
                         <div className="pDetails">
                             <h4 className="pTitle">Product Name</h4>
                             <p className="pDescription">Product description goes here.</p>
@@ -22,7 +22,7 @@ function Homepage() {
                     </section>
 
                     <section className="pCard">
-                        <img className="pImage" src="imgs/shirt.jpg" alt="Product Name" />
+                        <img className="pImage" src="./src/imgs/herp.jpg" alt="Product Name" />
                         <div className="pDetails">
                             <h4 className="pTitle">Product Name</h4>
                             <p className="pDescription">Product description goes here.</p>
@@ -32,7 +32,27 @@ function Homepage() {
                     </section>
 
                     <section className="pCard">
-                        <img className="pImage" src="imgs/shirt.jpg" alt="Product Name" />
+                        <img className="pImage" src="imgs/hero.jpg" alt="Product Name" />
+                        <div className="pDetails">
+                            <h4 className="pTitle">Product Name</h4>
+                            <p className="pDescription">Product description goes here.</p>
+                            <p className="pPrice">$99.99</p>
+                            <button className="add-to-cart">Add to Cart</button>
+                        </div>
+                    </section>
+
+                    <section className="pCard">
+                        <img className="pImage" src="imgs/hero.jpg" alt="Product Name" />
+                        <div className="pDetails">
+                            <h4 className="pTitle">Product Name</h4>
+                            <p className="pDescription">Product description goes here.</p>
+                            <p className="pPrice">$99.99</p>
+                            <button className="add-to-cart">Add to Cart</button>
+                        </div>
+                    </section>
+
+                    <section className="pCard">
+                        <img className="pImage" src="imgs/hero.jpg" alt="Product Name" />
                         <div className="pDetails">
                             <h4 className="pTitle">Product Name</h4>
                             <p className="pDescription">Product description goes here.</p>
