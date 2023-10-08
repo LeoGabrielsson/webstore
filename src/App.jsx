6770ab6481d6b84c0132b8f430ead3830a3fbc8a
Leo Gabrielsson <leo.gabrielsson@elev.ga.ntig.se>
2023-10-08 14:07:06 +0200

    Work from home

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
-import Homepage from './pages/Homepage'
-import Storepage from './pages/Storepage'
+import Homepage from './pages/Homepage.jsx'
+import Storepage from './pages/Storepage.jsx'
 
 function App() {
 
@@ -14,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Homepage />} />
           <Route path='/products' element={<Storepage />} />
+          <Route path='/your-cart' element={<Cartpage />} />
         </Routes>
       </main>
       <Footer />
---
+++ src/pages/Cartpage.jsx
@@ -0,0 +1,20 @@
+import "./Cartpage.css"
+
+function Cartpage() {
+    return (
+        <div className='mainSite'>
+            <div className="store">
+                <div>
+                    Lorem
+                </div>
+                <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nisi provident suscipit illum, magni sint voluptatibus distinctio optio assumenda aspernatur quisquam voluptatem atque. Possimus, minima excepturi pariatur sequi atque quisquam!
+                </div>
+
+            </div>
+            <button className="checkoutBtn">Checkout</button>
+        </div>
+    )
+}
+
+export default Cartpage
